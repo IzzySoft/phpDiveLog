9a9c8452702ca1da9c4010504c534edc98f3e7ee
Izzy <izzysoft@qumran.org>
2004-06-21 11:55:14 +0000

    - removed obsolete files - all data come out of the CSV files now
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpdivelog/trunk@31 87c0c0dd-7539-0410-b700-fc3a0562342c

--- stats.php
+++ stats.php
@@ -28,12 +28,27 @@
  $t->set_var("sites_tab_img",'<img src="'.$pdl->config->tpl_url.'images/globe.gif" width="15" height="15" border="0" alt="Globe" align="middle">');
  $t->set_var("sites_tab_name","Sites");
  $t->set_var("sites_ref","sitelist.php");
+ $t->set_var("tpl_dir",$pdl->config->tpl_url);
 
+ #==================================================[ Set up table headers ]===
+ $t->set_var("title","Dive Statistics");
+ $t->set_var("max_depth_name","Max Depth:");
+ $t->set_var("max_time_name","Max Divetime:");
+ $t->set_var("avg_depth_name","Avg Depth:");
+ $t->set_var("avg_time_name","Avg Divetime:");
+ $t->set_var("dive_num_name","Number of Dives:");
+ $t->set_var("cum_time_name","Cumulated Divetime:");
+
+ #================================================[ Import statistics data ]===
+ $stats = $pdl->db->get_stats();
+ $t->set_var("max_depth",$stats["max_depth"]);
+ $t->set_var("max_time",$stats["max_time"]);
+ $t->set_var("avg_depth",$stats["avg_depth"]);
+ $t->set_var("avg_time",$stats["avg_time"]);
+ $t->set_var("dive_num",$stats["dive_num"]);
+ $t->set_var("cum_time",$stats["cum_dive_time"]);
 
  $t->pparse("out","template");
-
- virtual("dives/index.html");
- echo "</td></tr></table>\n";
 
  include("inc/footer.inc");
 ?>
--- templates/default/stats.tpl
+++ templates/default/stats.tpl
@@ -20,4 +20,21 @@
 </table>
 
 </td></tr><tr class="td_transp"><td class="td_transp">
-<!--/table -->
+
+<table cellpadding="5" align="center" style="background-color:transparent;" border="0">
+ <tr class="td_blank"></tr>
+</table>
+<table border="0" cellpadding="4" align="center">
+ <tr><th colspan="5"><img src="{tpl_dir}images/btn_notes.gif" width="20" height="15" border="0" alt="Stats" align="middle"> {title}</th></tr>
+ <tr><th>{max_depth_name}</th><td align="right">{max_depth}</td>
+  <td class="td_blank"></td>
+  <th>{max_time_name}</th><td align="right">{max_time}</td></tr>
+ <tr><th>{avg_depth_name}</th><td align="right">{avg_depth}</td>
+  <th class="td_blank"></th>
+  <th>{avg_time_name}</th><td align="right">{avg_time}</td></tr>
+ <tr><th>{dive_num_name}</th><td align="right">{dive_num}</td>
+  <td class="td_blank"></td>
+  <th>{cum_time_name}</th><td>{cum_time}</td></tr>
+</table>
+
+</td></tr></table>
