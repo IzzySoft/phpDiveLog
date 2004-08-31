0f61bc5fa8a568c8bccae635a78f8fac63b4c146
Izzy <izzysoft@qumran.org>
2004-08-31 15:36:38 +0000

    + added rsync support
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpdivelog/trunk@172 87c0c0dd-7539-0410-b700-fc3a0562342c

--- install/adl/divelog_conduit.sh
+++ install/adl/divelog_conduit.sh
@@ -24,10 +24,13 @@ USELOCAL=1
 # the location of phpDiveLogs user dir (where the data/ and images/ dirs are)
 PDLBASE=/web/divelog/diver/demo
 #-----------------------------------------------------[ Remote Transfers ]---
-# Do we intend to use SCP transfers? [0|1]
+# Do we intend to use SCP transfers? [0|1|2] (0=No,1=SCP,2=RSync)
 USESCP=0
 # If so, we need the target base directory
 SCPBASE=user@machine:/path_to_PDL/diver/demo
+# for RSync, specify whether to copy from your local PDL installation (1 - needs
+# USELOCAL=1) or from the ADL logdir (2)
+RSYNCBASE=1
 
 #=========================================================[ Intro Output ]===
 echo "
@@ -51,18 +54,36 @@ done
 echo `date +"$DATEFORMAT"` "Converting AquaDiveLog Data..."
 java -jar conduit.jar -$UNITS $* >/dev/null
 
-# SCP transfer to REMOTE machine
-if [ $USESCP -eq 1 ]; then
-  echo `date +"$DATEFORMAT"` "Copying files to the remote web target dir..."
-  scp $LOGDIR/csv/* $SCPBASE/data &>/dev/null
-  scp $LOGDIR/images/* $SCPBASE/images/ &>/dev/null
-fi
-
 # transfer CSV files to LOCAL web target
 if [ $USELOCAL -eq 1 ]; then
   echo `date +"$DATEFORMAT"` "Copying files to the local web target dir..."
   cp $LOGDIR/csv/* $PDLBASE/data/ &>/dev/null
   cp $LOGDIR/images/* $PDLBASE/images/ &>/dev/null
+fi
+
+# SCP transfer to REMOTE machine
+if [ $USESCP -eq 1 ]; then
+  echo `date +"$DATEFORMAT"` "Copying files to the remote web target dir via SCP..."
+  scp $LOGDIR/csv/* $SCPBASE/data &>/dev/null
+  scp $LOGDIR/images/* $SCPBASE/images/ &>/dev/null
+
+# RSync with REMOTE machine
+elif [ $USESCP -eq 2 ]; then
+  if [ $RSYNCBASE -eq 1 ]; then
+    RLOGDIR=$PDLBASE
+  else
+    RLOGDIR=$LOGDIR
+  fi
+  echo `date +"$DATEFORMAT"` "Syncronizing with remote web target dir via RSync..."
+  echo `date +"$DATEFORMAT"` "- Sync DataFiles..."
+  rsync -ae ssh $RLOGDIR/csv $SCPBASE/data &>/dev/null
+  echo `date +"$DATEFORMAT"` "- Sync DiveProfiles..."
+  rsync -ae ssh $RLOGDIR/images $SCPBASE/images &>/dev/null
+  echo `date +"$DATEFORMAT"` "- Sync Fotos..."
+  rsync -ae ssh $RLOGDIR/fotos/* $SCPBASE/fotos &>/dev/null
+  echo `date +"$DATEFORMAT"` "- Sync TextFiles..."
+  rsync -ae ssh $RLOGDIR/notes/* $SCPBASE/notes &>/dev/null
+  rsync -ae ssh $RLOGDIR/text/* $SCPBASE/text &>/dev/null
 fi
 
 # Finito
