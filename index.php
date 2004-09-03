8d3293fe2a9e40a4fbde64f7983d6276cafb6394
Izzy <izzysoft@qumran.org>
2004-09-03 13:06:10 +0000

    + added first/last buttons to the navigation in dive[site] lists
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpdivelog/trunk@186 87c0c0dd-7539-0410-b700-fc3a0562342c

--- index.php
+++ index.php
@@ -35,15 +35,19 @@
  if ($start) {
    $prev = $start - $pdl->config->display_limit;
    if ($prev<0) $prev=0;
-   $t->set_var("nav_left",$pdl->link->linkurl($_SERVER["SCRIPT_NAME"]."?start=$prev","<img src='".$pdl->config->tpl_url."images/left.gif'>"));
+   $first = $pdl->link->linkurl($_SERVER["SCRIPT_NAME"]."?start=0","<img src='".$pdl->config->tpl_url."images/first.gif'>");
+   $t->set_var("nav_left",$first.$pdl->link->linkurl($_SERVER["SCRIPT_NAME"]."?start=$prev","<img src='".$pdl->config->tpl_url."images/left.gif'>"));
  } else {
-   $t->set_var("nav_left","<img src='".$pdl->config->tpl_url."images/left-grey.gif'>");
+   $first = "<img src='".$pdl->config->tpl_url."images/first-grey.gif'>";
+   $t->set_var("nav_left","$first<img src='".$pdl->config->tpl_url."images/left-grey.gif'>");
  }
  if ($records - $start < $pdl->config->display_limit) {
-   $t->set_var("nav_right","<img src='".$pdl->config->tpl_url."images/right-grey.gif'>");
+   $last = "<img src='".$pdl->config->tpl_url."images/last-grey.gif'>";
+   $t->set_var("nav_right","<img src='".$pdl->config->tpl_url."images/right-grey.gif'>$last");
  } else {
+   $last = $pdl->link->linkurl($_SERVER["SCRIPT_NAME"]."?start=".floor($records/$pdl->config->display_limit)*$pdl->config->display_limit,"<img src='".$pdl->config->tpl_url."images/last.gif'>");
    $next = $start + $pdl->config->display_limit;
-   $t->set_var("nav_right",$pdl->link->linkurl($_SERVER["SCRIPT_NAME"]."?start=$next","<img src='".$pdl->config->tpl_url."images/right.gif'>"));
+   $t->set_var("nav_right",$pdl->link->linkurl($_SERVER["SCRIPT_NAME"]."?start=$next","<img src='".$pdl->config->tpl_url."images/right.gif'>$last"));
  }
 
  #===============================================[ set up the table header ]===
