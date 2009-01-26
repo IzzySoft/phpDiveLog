00655db901cc8d2a99b6e587b92098236ae5ff4f
Izzy <izzysoft@qumran.org>
2009-01-26 12:01:44 +0000

    ! fixing non-DMS GPS data for sitelist KML export
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpdivelog/trunk@349 87c0c0dd-7539-0410-b700-fc3a0562342c

--- sitelist_kml.php
+++ sitelist_kml.php
@@ -1,6 +1,6 @@
 <?
  #############################################################################
- # phpDiveLog                               (c) 2004-2008 by Itzchak Rehberg #
+ # phpDiveLog                               (c) 2004-2009 by Itzchak Rehberg #
  # written by Itzchak Rehberg <izzysoft AT qumran DOT org>                   #
  # http://www.izzysoft.de/                                                   #
  # ------------------------------------------------------------------------- #
@@ -21,28 +21,6 @@
  $pdl_url = $pdl->link->get_baseurl();
  $img_url = $pdl_url."/".$pdl->config->tpl_url."images/";
 
- #-=[ Convert coordinates to decimal ]=-
- function mk_coord($str) {
-   if ( preg_match("/([0-9]+)[^0-9]+([0-9]+)[^0-9.]+([0-9.]+)[^0-9]+/",$str,$match) ) {
-     $code[0] = $match[1]; $code[1] = $match[2]; $code[2] = $match[3];
-   } elseif ( preg_match("/([0-9]+)[^0-9]+([0-9.]+)[^0-9.]+/",$str,$match) ) {
-     $code[0] = $match[1]; $code[1] = floor($match[2]);
-     $code[2] = ($match[2] - floor($match[2])) * 60;
-   } elseif ( preg_match("/([0-9.]+)[^0-9.]+/",$str,$match) ) {
-     $code[0] = floor($match[1]);
-     $code[1] = floor( ($match[1] - $code[0]) * 60);
-     $code[2] = floor( (($match[1] - $code[0]) * 60 - $code[1]) * 60 );
-   }
-   if ( substr($str,strlen($str)-1)=="N" || substr($str,strlen($str)-1)=="E" ) {
-     $code[4] = 1;
-   } else {
-     $code[4] = -1;
-   }
-   $min  = $code[1] + $code[2]/60;
-   $coord = ($code[0] + $min/60) * $code[4];
-   return $coord;
- }
-
  #-=[ Setup Template ]=-
  $t = new Template($pdl->config->tpl_path);
  $t->set_file(array("template"=>"sitelist_kml.tpl"));
@@ -56,7 +34,6 @@
    $sort  = $pdl->config->sitelist_default_sort;
    $order = $pdl->config->sitelist_default_order;
  }
-# $sites = $pdl->db->get_sites($start,$pdl->config->display_limit,FALSE,$sort,$order);
  $sites = $pdl->db->get_sites($start,999999,FALSE,$sort,$order);
  $max   = count($sites);
  $records = $pdl->db->sites;
@@ -69,8 +46,8 @@
  $t->set_var("viewingdistance","1000");
  $details = array ("altitude","description");
  for ($i=0;$i<$max;++$i) {
-   $latitude  = mk_coord($sites[$i]["latitude"]);
-   $longitude = mk_coord($sites[$i]["longitude"]);
+   $latitude  = $pdl->link->coord2dec($sites[$i]["latitude"]);
+   $longitude = $pdl->link->coord2dec($sites[$i]["longitude"]);
    if ($latitude == 0 || $longitude == 0) continue;
    $depth = $sites[$i]["depth"]; # if (!empty($depth)) $depth .= "m";
    $loc = $sites[$i]["loc"].": ".$sites[$i]["place"];
